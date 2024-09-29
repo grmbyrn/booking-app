59c5a1fb300a90e6adb9374063b25303f5f66819
Graeme Byrne <grmbrn89@gmail.com>
2024-09-29 08:45:12 +0200

    add single PlacePage component

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,4 +1,5 @@
 import axios from "axios"
+import { Link } from "react-router-dom"
 import { useEffect, useState } from "react"
 
 const IndexPage = () => {
@@ -11,7 +12,7 @@ const IndexPage = () => {
   return (
     <div  className="mt-8 grid gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {places.length > 0 && places.map(place => (
-        <div key={place._id}>
+        <Link to={'/place/'+place._id} key={place._id}>
           <div className="bg-gray-500 mb-2 rounded-2xl flex">
             {place.photos?.[0] && (
               <img className="object-cover aspect-square" src={'http://localhost:4000/uploads/' + place.photos?.[0]} alt="" />
@@ -24,7 +25,7 @@ const IndexPage = () => {
           <div className="mt-1">
             <span className="font-bold">${place.price}</span> per night
           </div>
-        </div>
+        </Link>
       ))}
     </div>
   )
---
+++ client/src/pages/PlacePage.jsx
@@ -0,0 +1,46 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+
+const PlacePage = () => {
+    const {id} = useParams()
+    const [place, setPlace] = useState(null)
+    useEffect(() => {
+        if(!id){
+            return
+        }
+        axios.get(`/places/${id}`).then(response => {
+            console.log(response.data.photos)
+            setPlace(response.data)
+        })
+    }, [id])
+
+    if(!place) return ''
+  return (
+    <div className="mt-4 bg-gray-100 -mx-8 px-8 py-8">
+      <h1 className="text-3xl">{place.title}</h1>
+      <a className="my-2 block font-semibold" target="_blank" href={'https://maps.google.com/?q='+place.address}>{place.address}</a>
+      <div className="grid gap-2 grid-cols-[2fr_1fr]">
+        <div>
+            {place.photos?.[0] && (
+                <div>
+                    <img className="aspect-square object-cover" src={'http://localhost:4000/uploads/'+place.photos[0]} alt="" />
+                </div>
+            )}
+        </div>
+        <div className="grid">
+            {place.photos?.[1] && (
+                <img className="aspect-square object-cover" src={'http://localhost:4000/uploads/'+place.photos[1]} alt="" />
+            )}
+            <div className="overflow-hidden">
+                {place.photos?.[2] && (
+                    <img className="aspect-square object-cover relative top-2" src={'http://localhost:4000/uploads/'+place.photos[2]} alt="" />
+                )}
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PlacePage
