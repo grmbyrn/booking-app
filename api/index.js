b00499dc12948ed058ce697d16adfd282fe4457f
Graeme Byrne <grmbrn89@gmail.com>
2024-10-06 12:27:05 +0200

    add /api to endpoints in index.js

--- api/index.js
+++ api/index.js
@@ -68,12 +68,12 @@ function getUserDataFromReq(req){
     })
 }
 
-app.get('/test', (req, res) => {
+app.get('/api/test', (req, res) => {
     mongoose.connect(process.env.MONGO_URL)
     res.json('test ok')
 })
 
-app.post('/register' , async(req, res) => {
+app.post('/api/register' , async(req, res) => {
     mongoose.connect(process.env.MONGO_URL)
     const {name, email, password} = req.body
 
@@ -90,7 +90,7 @@ app.post('/register' , async(req, res) => {
     }
 })
 
-app.post('/login', async (req, res) => {
+app.post('/api/login', async (req, res) => {
     mongoose.connect(process.env.MONGO_URL)
     const {email, password} = req.body
     const userDoc = await User.findOne({email})
@@ -112,7 +112,7 @@ app.post('/login', async (req, res) => {
     }
 })
 
-app.get('/profile', (req, res) => {
+app.get('/api/profile', (req, res) => {
     mongoose.connect(process.env.MONGO_URL)
     const {token} = req.cookies
     if(token){
@@ -126,11 +126,11 @@ app.get('/profile', (req, res) => {
     }
 })
 
-app.post('/logout', (req, res) => {
+app.post('/api/logout', (req, res) => {
     res.cookie('token', '').json(true)
 })
 
-app.post('/upload-by-link', async (req, res) => {
+app.post('/api/upload-by-link', async (req, res) => {
     const {link} = req.body
     const newName = 'photo' + Date.now() + '.jpg'
     await imageDownLoader.image({
@@ -142,7 +142,7 @@ app.post('/upload-by-link', async (req, res) => {
 })
 
 const photosMiddleware = multer({dest: '/tmp'})
-app.post('/upload', photosMiddleware.array('photos', 100), async (req, res) => {
+app.post('/api/upload', photosMiddleware.array('photos', 100), async (req, res) => {
     const uploadedFiles = []
     for (let i = 0; i < req.files.length; i++) {
         const {path, originalname, mimetype} = req.files[i]
@@ -152,7 +152,7 @@ app.post('/upload', photosMiddleware.array('photos', 100), async (req, res) => {
     res.json(uploadedFiles)
 })
 
-app.post('/places', (req, res) => {
+app.post('/api/places', (req, res) => {
     mongoose.connect(process.env.MONGO_URL)
     const {token} = req.cookies
     const {title, address, addedPhotos, description, perks, extraInfo, checkIn, checkOut, maxGuests, price} = req.body
@@ -166,7 +166,7 @@ app.post('/places', (req, res) => {
     })
 })
 
-app.get('/user-places', (req, res) => {
+app.get('/api/user-places', (req, res) => {
     mongoose.connect(process.env.MONGO_URL)
     const {token} = req.cookies
     jwt.verify(token, jwtSecret, {}, async (err, userData) => {
@@ -175,13 +175,13 @@ app.get('/user-places', (req, res) => {
     }
 )})
 
-app.get('/places/:id', async(req, res) => {
+app.get('/api/places/:id', async(req, res) => {
     mongoose.connect(process.env.MONGO_URL)
     const {id} = req.params
     res.json(await Place.findById(id))
 })
 
-app.put('/places/:id', async (req, res) => {
+app.put('/api/places/:id', async (req, res) => {
     mongoose.connect(process.env.MONGO_URL)
     const { token } = req.cookies;
     const { title, address, addedPhotos, description, perks, extraInfo, checkIn, checkOut, maxGuests, price } = req.body;
@@ -220,12 +220,12 @@ app.put('/places/:id', async (req, res) => {
     });
 });
 
-app.get('/places', async (req, res) => {
+app.get('/api/places', async (req, res) => {
     mongoose.connect(process.env.MONGO_URL)
     res.json(await Place.find())
 })
 
-app.post('/bookings', async (req, res) => {
+app.post('/api/bookings', async (req, res) => {
     mongoose.connect(process.env.MONGO_URL)
     try {
         const userData = await getUserDataFromReq(req); // Ensure this function works and returns the user data
@@ -247,7 +247,7 @@ app.post('/bookings', async (req, res) => {
 });
 
 
-app.get('/bookings', async (req, res) => {
+app.get('/api/bookings', async (req, res) => {
     mongoose.connect(process.env.MONGO_URL)
     const userData = await getUserDataFromReq(req)
     res.json(await Booking.find({user:userData.id}).populate('place'))
